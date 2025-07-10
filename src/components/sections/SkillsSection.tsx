922afa0679e09555b75e641b6560658c8ec845bc
Aveev Bair <aveevb123@gmail.com>
2025-07-10 16:20:44 +0300

    refactor: refactor skills image

--- src/components/sections/SkillsSection.tsx
+++ src/components/sections/SkillsSection.tsx
@@ -14,7 +14,7 @@ const MySkills = () => {
         <div className="w-full flex flex-col gap-8">
           <div className="w-full flex justify-center items-center">
             <Image
-              src="https://skillicons.dev/icons?i=js,html,css,figma,git,github,nextjs,react,sass,tailwind,ts,&perline=15"
+              src="https://skillicons.dev/icons?i=js,html,css,figma,git,github,nextjs,react,sass,tailwind,materialui,ts,npm,redux&perline=15"
               alt="My Skills"
               className="relative w-full h-full max-w-3xl"
               width={500}
